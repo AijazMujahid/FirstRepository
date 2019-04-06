1c40ae4ac18c489d42eec711f1af98ae4d50debd
Mujahiduddin Mohammed <mmohamme@ipsoft.com>
2019-04-07 03:25:18 +0530

    practice1 4apr

---
+++ Practice1/Parent.java
@@ -0,0 +1,15 @@
+public class Parent {
+
+	public static void main(String[] args) {
+
+		System.out.println("Parent Class Main");
+	}
+
+}
+
+class Child extends Parent {
+
+	public static void main(String[] args) {
+		System.out.println("Child Class Main");
+	}
+}
